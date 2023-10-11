8029f0fa2c5c2ee21edd1d9fa019b1800756aad5
yong <e2yong98@gmail.com>
2023-10-11 23:55:32 +0900

    Test Commit

---
+++ src/main/java/capstone3/createppt/CreatePptApplication.java
@@ -0,0 +1,13 @@
+package capstone3.createppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatePptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatePptApplication.class, args);
+	}
+
+}
