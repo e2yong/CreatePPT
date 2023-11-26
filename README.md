115908bfdc3aa472c42c56ad918d23351bfbfbe4
yong <e2yong98@gmail.com>
2023-11-26 22:34:49 +0900

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# CreatePPT
+Spring Boot로 구현한 발표 자료 생성 웹사이트
+
+test
---
+++ create-ppt/src/main/java/capstone3/createppt/CreatePptApplication.java
@@ -0,0 +1,15 @@
+package capstone3.createppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatePptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatePptApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
